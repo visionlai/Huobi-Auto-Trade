6622282acf9ceb1f3978b924a9565e069adc5017
visionlai <1874560651@qq.com>
2020-08-27 19:39:39 +0800

    init by uploading offical codes

---
+++ huobi/connection/impl/websocket_request.py
@@ -0,0 +1,13 @@
+from huobi.constant import ApiVersion
+
+
+class WebsocketRequest(object):
+
+    def __init__(self):
+        self.subscription_handler = None
+        self.auto_close = False   # close connection after receive data, for subscribe set False, for request set True
+        self.is_trading = False
+        self.error_handler = None
+        self.json_parser = None
+        self.update_callback = None
+        self.api_version = ApiVersion.VERSION_V1  # v1 as default
